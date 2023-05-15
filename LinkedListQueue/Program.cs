655c615ba421e7878f4775a6b5767f620d56cc7d
Barash Serbest <barisserbest127@gmail.com>
2023-05-15 19:33:08 +0300

    Linked List Queue
    Barash Serbest / 5220505051 - C#

---
+++ LinkedListQueue/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kuyruk = new Queue(); // "kuyruk" isminde "Queue" Class türünden değişken tanımlandı. "Queue" üzerinden "F12" tuşu ile Class içerisine gidip kod incelenebilir. 
+            int tercih;
+
+            do // Kullanıcıdan girilen değer 4'ten farklı olduğu süre boyunca işlem devam edecek. 
+            {
+                Console.WriteLine("Lütfen aşağıdaki işlemlerden birini seçiniz:");
+                Console.WriteLine("1. Ekleme (Enqueue)");
+                Console.WriteLine("2. Silme (Dequeue)");
+                Console.WriteLine("3. Kuyruğu Görüntüle");
+                Console.WriteLine("4. Çıkış");
+                Console.Write("Seçiminizi yapın: ");
+                tercih = int.Parse(Console.ReadLine());
+
+                switch (tercih) // Kullanıcıdan alınan değere karşılık gelen işlemi gerçekleştirme. 
+                {
+                    case 1: 
+                        Console.Write("Eklenecek elemanı girin: ");
+                        int eleman = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        kuyruk.Enqueue(eleman);
+                        break;
+                    case 2:
+                        kuyruk.Dequeue();
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        kuyruk.PrintQueue();
+                        break;
+                    case 4:
+                        Console.WriteLine("\nProgram sonlandırıldı, lütfen bir tuşa basarak çıkış yapınız.");
+                        break;
+                    default:
+                        Console.WriteLine("Geçersiz seçenek. Tekrar deneyin.");
+                        break;
+                }
+                Console.WriteLine();
+            } while (tercih != 4);
+        }
+    }
+
+}
---
+++ LinkedListQueue/Queue.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace LinkedListQueue
+{
+    
+class Node
+    {
+        public int Data;
+        public Node Next;
+    }
+class Queue
+    {
+        private Node front;
+        private Node rear;
+
+        public Queue()
+        {
+            front = null;
+            rear = null;
+        }
+
+        public void Enqueue(int data) // Geriye değer döndürmeyecek şekilde "void" tanımlandı.
+        {
+            Node newNode = new Node();
+            newNode.Data = data;
+            newNode.Next = null;
+
+            if (rear == null)
+            {
+                front = newNode;
+                rear = newNode;
+            }
+            else
+            {
+                rear.Next = newNode;
+                rear = newNode;
+            }
+
+            Console.WriteLine($"Girdiğiniz {data} elemanı kuyruğa eklendi.");
+        }
+
+        public void Dequeue() // Geriye değer döndürmeyecek şekilde "void" tanımlandı.
+        {
+            if (front == null)
+            {
+                Console.WriteLine("Kuyruk boş. Eleman çıkarılamaz.");
+                return;
+            }
+
+            int removedData = front.Data;
+            front = front.Next;
+
+            if (front == null)
+                rear = null;
+
+            Console.Write($"\nÇıkarılan eleman: {removedData}");
+        }
+
+        public void PrintQueue() // Geriye değer döndürmeyecek şekilde "void" tanımlandı.
+        {
+            if (front == null)
+            {
+                Console.WriteLine("Kuyruk boş.");
+                return;
+            }
+
+            Console.Write("\nKuyruk elemanları: ");
+            Node current = front;
+            while (current != null)
+            {
+                Console.Write($"{current.Data} ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
